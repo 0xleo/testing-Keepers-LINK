d1668f61661a9d41815ab5008cb4f1d15674adfa
leo.dev <leojim2002@gmail.com>
2023-07-28 03:10:18 -0400

    Initial commit

---
+++ contract.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.6;
+
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/KeeperCompatibleInterface.sol";
+
+contract Counter is KeeperCompatibleInterface {
+    uint public counter = 0;
+    uint public immutable interval = 60;
+    uint public lastTimeStamp = block.timestamp;
+
+    function checkUpkeep(bytes calldata checkData) public pure override returns (bool upkeepNeeded, bytes memory performData) {
+        upkeepNeeded = (block.timestamp - lastTimeStamp) > interval;
+        performData = checkData;
+    }
+
+    function performUpkeep(bytes calldata performData) external override {
+        lastTimeStamp = block.timestamp;
+        counter = counter + 1;
+        performData;
+    }
+}
